33865c0863e43c6b4397599e06c8515331b5ae86
AnhLe3698 <anhviet.le00@hotmail.com>
2022-10-17 15:04:34 -0600

    Remove none-used files

--- public/scripts/htmlForms.js
+++ public/scripts/htmlForms.js
@@ -99,7 +99,6 @@ let listForms = `
           const formData = new FormData(document.querySelector('#remove-item'))
           const errorString = 'Invalid item';
           let itemName = formData.get('itemName');
-          console.log('clicked and detected');
           let urlStr = '/users/delete/' + itemName;
           if (itemName.length !== 0) {
             $.post(urlStr).done(function (data) {
