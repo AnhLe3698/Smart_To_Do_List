08f7e46011730bd734a2068d0a28758c5e282a6f
AnhLe3698 <anhviet.le00@hotmail.com>
2022-10-17 14:29:24 -0600

    Refactor files and functions of frontend

--- public/scripts/addItem.js
+++ public/scripts/addItem.js
@@ -1,4 +1,3 @@
-
 $('#add-item2').submit((event) => {
   event.preventDefault();
   const formData = new FormData(document.querySelector('#add-item2'))
@@ -8,6 +7,43 @@ $('#add-item2').submit((event) => {
   console.log('name clicked')
   if (name.length !== 0) {
     $.post('/users/add', { 'name': name }).done(function (data) {
+      const $data = data;
+      if ($data === errorString) {
+        $('main').append($data);
+      } else {
+        if (data.category === 'movie') {
+          $('.media').append(listItems($data));
+        } else if (data.category === 'book') {
+          $('.books').append(listItems($data));
+        } else if (data.category === 'product') {
+          $('.products').append(listItems($data));
+        } else if (data.category === 'restaurant') {
+          $('.resteraunts').append(listItems($data));
+        };
+      }
+    });
+  }
+});
+
+{/* <form id="add-item">
+  <div class="form-group">
+    <label for="name">Enter item name</label>
+    <input class="form-control" type="text" name="name" placeholder="Add name" style="width: 300px">
+    <label for="category">Enter item category</label>
+    <input class="form-control" type="text" name="category" placeholder="Add category" style="width: 300px">
+  </div>
+  <button type="submit" class="btn btn-primary">Add</button>
+</form> */}
+
+$('#add-item').submit((event) => {
+  event.preventDefault();
+  const formData = new FormData(document.querySelector('#add-item'))
+  const errorString = 'Sorry item already exists';
+  let name = formData.get('name');
+  let category = formData.get('category');
+
+  if (name.length !== 0 && category.length !== 0) {
+    $.post('/users/insert', { 'name': name, 'category': category }).done(function (data) {
       const $data = data;
       if ($data === errorString) {
         $('main').append($data);
---
+++ public/scripts/htmlForms.js
@@ -0,0 +1,172 @@
+const listItems = function (items) {
+  let markup = `
+    <li class="dropzone list-group" id=${items.id} draggable="true">${items.name}</li>
+     `;
+  return markup;
+}
+
+const loginForm = `
+      <h3>Login</h3>
+      <form id="logging-in" action="/login" method="POST">
+        <div class="form-group">
+          <label for="email">Email address</label>
+          <input class="form-control" type="email" name="email" placeholder="Enter email" style="width: 300px">
+        </div>
+        <button type="submit" class="btn btn-primary">Login</button>
+      </form>
+      <script>
+      $('#logging-in').submit((event) => {
+        event.preventDefault();
+        const formData = new FormData(document.querySelector('#logging-in'))
+        const errorString = 'Invalid email';
+        $('ul').empty();
+        $.post('/users/login', { email: formData.get('email') }).done(function (data) {
+          // Data we get back from the server
+          const $data = data;
+          if ($data === errorString) {
+            $('main').append($data);
+          } else {
+            $('main').empty();
+            $('main').append(listForms);
+            data.map(item => {
+              const $item = listItems(item);
+              const category = item.category;
+              if (category === 'movie') {
+                $('.media').append($item);
+              } else if (category === 'book') {
+                $('.books').append($item);
+              } else if (category === 'product') {
+                $('.products').append($item);
+              } else if (category === 'restaurant') {
+                $('.resteraunts').append($item);
+              }
+            });
+          }
+        });
+      });
+      </script>
+    `;
+// Creates the list items
+let listForms = `<section class="toDoListBox box">
+      <section class="toWatch toDoBox box">
+        <header class="toDoHeader">To Watch Section</header>
+        <section>
+          <ul class="list-group media">
+
+        </section>
+      </section>
+
+
+      <section class="toEat toDoBox box">
+        <header class="toDoHeader">To Eat Section</header>
+        <section class="list-container">
+          <ul class="list-group resteraunts">
+
+        </section>
+      </section>
+
+      <section class="toRead toDoBox box">
+        <header class="toDoHeader">To Read Section</header>
+        <section>
+          <ul class="list-group books">
+
+
+        </section>
+      </section>
+
+      <section class="toBuy toDoBox box">
+        <header class="toDoHeader">To Buy Section</header>
+        <section>
+          <ul class="list-group products">
+
+
+          </ul>
+        </section>
+      </section>
+      </section>
+
+      <h3>Add item Part 2</h3>
+    <form id="add-item2">
+      <div class="form-group">
+        <label for="name">Enter item name</label>
+        <input class="form-control" type="text" name="name" placeholder="Add name" style="width: 300px">
+      </div>
+      <button type="submit" class="btn btn-primary">Add</button>
+    </form>
+
+
+    <h3>Remove item</h3>
+    <form id="remove-item">
+      <div class="form-group">
+        <label for="itemName">Enter item name</label>
+        <input class="form-control" type="text" name="itemName" placeholder="Add item Name" style="width: 300px">
+      </div>
+      <button type="submit" class="btn btn-primary">remove</button>
+    </form>
+    `;
+
+
+const registerForm = `
+      <h3>Register</h3>
+      <form id="register-user" action="/register" method="POST">
+        <div class="form-group">
+          <label for="email">Email address</label>
+          <input class="form-control" type="email" name="email" placeholder="Enter email" style="width: 300px">
+          <label for="first-name">First Name</label>
+          <input class="form-control" type="name" name="first-name" placeholder="Enter first Name" style="width: 300px">
+          <label for="first-name">Last Name</label>
+          <input class="form-control" type="name" name="last-name" placeholder="Enter Last Name" style="width: 300px">
+        </div>
+        <button type="submit" class="btn btn-primary">Register</button>
+      </form>
+      <script>
+      $('#register-user').submit((event) => {
+        event.preventDefault();
+        const formData = new FormData(document.querySelector('#register-user'))
+        const errorString = 'Invalid email';
+        let email = formData.get('email');
+        let firstName = formData.get('first-name');
+        let lastName = formData.get('last-name');
+        console.log(email, firstName, lastName);
+        if (email.length !== 0 && firstName.length !== 0 && lastName.length !== 0) {
+          $.post('/users/register', { 'email': email, 'firstName': firstName, 'lastName': lastName }).done(function (data) {
+            const $data = data;
+            if ($data === errorString) {
+              $('main').append($data);
+            } else {
+              $('main').append($data);
+              // $('main').append('Login Successful');
+            }
+          });
+        }
+      });
+      </script>
+      `;
+
+$(document).ready(function() {
+
+      $(function() {
+        $.get('/users', (data) => {
+          const errorString = 'Not logged in';
+          const $data = data;
+          if ($data === errorString) {
+            $('main').append($data);
+          } else {
+            $('main').append(listForms);
+            data.map(item => {
+              const $item = listItems(item);
+              const category = item.category;
+              if (category === 'movie') {
+                $('.media').append($item);
+              } else if (category === 'book') {
+                $('.books').append($item);
+              } else if (category === 'product') {
+                $('.products').append($item);
+              } else if (category === 'restaurant') {
+                $('.resteraunts').append($item);
+              }
+            });
+          }
+        });
+      })
+});
