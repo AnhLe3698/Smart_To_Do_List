991c87fe13020eebae7f8a5eba42e0fa224252e1
AnhLe3698 <anhviet.le00@hotmail.com>
2022-10-17 14:32:57 -0600

    refactor server.js and add comments

--- server/server.js
+++ server/server.js
@@ -7,15 +7,15 @@ const express = require('express');
 const morgan = require('morgan');
 const cookiesState = require('cookie-parser');
 
-
+// Setting port and creating an instant of a server
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-app.set('view engine', 'ejs');
-
+// Get and Post request statistics
 app.use(morgan('dev'));
 app.use(cookiesState());
 app.use(express.urlencoded({ extended: true }));
+// Sass to css constructor
 app.use(
   '/styles',
   sassMiddleware({
@@ -28,14 +28,11 @@ app.use(
 // Express will serve Frontend Files from /public folder
 app.use(express.static('public'));
 
-
+// Our routes are extensions from /users path
 const usersRoutes = require('./routes/users');
-
 app.use('/users', usersRoutes);
 
-
-
-
+// Listener
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
